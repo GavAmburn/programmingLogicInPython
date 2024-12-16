5f88801a14f04af55a8e5c4a17bf2839fadabfaa
Sthefanie <sthefaniepasso@gmail.com>
2024-12-16 17:32:59 -0600

    area and perimeter of a square

--- geometric_formula.py
+++ geometric_formula.py
@@ -1,3 +1,10 @@
 # Calculate the perimeter and area of the geometric elements: https://i.pinimg.com/736x/d5/e7/c1/d5e7c1535e1bd4136f3bac08b3304135.jpg
 
 #git pull
+print("**********************************")
+side = float(input("Digit the side of the square:"))
+perimeter = side*4 
+area = side*side
+print("Square Perimeter: ", perimeter)
+print("Square Area:", area)
+print("**********************************")
