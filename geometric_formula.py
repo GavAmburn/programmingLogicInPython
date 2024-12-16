751bc137a58f63dcfac45772da8b599f01dd8151
Gav <gav@Gavs-Air.lan>
2024-12-16 17:35:04 -0600

    Area and perimieter of a CIRCLE

--- geometric_formula.py
+++ geometric_formula.py
@@ -1,3 +1,11 @@
 # Calculate the perimeter and area of the geometric elements: https://i.pinimg.com/736x/d5/e7/c1/d5e7c1535e1bd4136f3bac08b3304135.jpg
 
 #git pull
+
+radius = int(input("Input your radius"))
+
+perimeter_circle = radius * 2 * 3.14
+area_circle = 3.14 * radius ** 2
+
+print ("Your perimeter is: ", perimeter_circle)
+print ("Your area is: ", area_circle)
